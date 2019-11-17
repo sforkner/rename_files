09cf2c59563593776b61d6e77a6857a58e776ca8
SForkner <sforkner@juno.com>
2019-11-17 14:06:04 -0500

    working

--- rename.py
+++ rename.py
@@ -1,11 +1,13 @@
 import sys
 import os
 
-path = smb: // sam-desktop.local/sam/downloadsjd/The % 20New % 20York % 20Times % 20Best % 20Sellers % 20(Fiction)
+# path = smb: // sam-desktop.local/sam/downloadsjd/"The % 20New % 20York % 20Times % 20Best % 20Sellers % 20(Fiction)"
 
 # grab first snd second arguments
-old_folder = smb: // sam-desktop.local/sam/downloadsjd/The % 20New % 20York % 20Times % 20Best % 20Sellers % 20(Fiction)
-new_folder = smb: // sam-desktop.local/sam/downloadsjd/best_sellers_list_new/
+old_folder = '/home/sam/Public/The New York Times Best Sellers (Fiction)'
+
+new_folder = '/home/sam/Public/TheNewYorkTimesBestSellers(Fiction)'
+
 
 # check if new_folder exists, if not create it
 if not os.path.exists(new_folder):
@@ -13,16 +15,16 @@ if not os.path.exists(new_folder):
 
 # loop through  old_folder
 for filename in os.listdir(old_folder):
-    # img = Image.open(f'{old_folder}{filename}')
-    # clean_name = os.path.splitext(filename)[0]
-    # img.save(f'{new_folder}{clean_name}.png', 'png')
-    # print(f'Done {clean_name}')
-
-    file = open(f'{old_folder}{filename}')
-    clean_name = os.path.splitext(filename)[0]
+    file = open(f'{old_folder}/{filename}', mode='rb')
+    data = file.read()
+    file.close()
+    clean_name = filename.split('. ', 1)[1]
     print(filename)
+
     print(clean_name)
-    save(f'{new_folder}{clean_name}')
+    new_file = open(f'{new_folder}/{clean_name}', mode='wb')
+    new_file.write(data)
+    new_file.close()
+
     print(f'Done {clean_name}')
-    print(file)
 print(" All Done")
